27492491c5b92cc164acf16fa7c918068f80eb97
ihsanshiddiq <ihsanshiddiq6513@gmail.com>
2021-01-14 04:21:14 +0800

    Upd8 v0.85; early access final version

---
+++ src/nitestay/Nitestay.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nitestay;
+
+/**
+ *
+ * @author Ceruleaf
+ */
+public class Nitestay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginView loginview = new LoginView();
+        loginview.setLocationRelativeTo(null);
+        loginview.setVisible(true);
+    }
+    
+}
